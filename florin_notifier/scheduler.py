891cd4dd4e5bf18246a3851d52003ed936dc66e0
Kevin Jing Qiu <kevin@idempotent.ca>
2017-11-25 00:37:22 -0500

    Configuration

--- florin_notifier/scheduler.py
+++ florin_notifier/scheduler.py
@@ -1,4 +1,6 @@
+import os
 import logging
+import yaml
 from celery import Celery
 from celery.schedules import crontab
 from .tasks import notify_new_transactions as _notify_new_transactions
@@ -15,6 +17,17 @@ logging.basicConfig(level='INFO')
 
 @app.on_after_configure.connect
 def setup(sender, **kwargs):
-    sender.add_periodic_task(
-        crontab(minute="*/1"),
-        notify_new_transactions.s('Rogers Mastercard', 'rogersbank.json.gpg', 'kevin.jing.qiu@gmail.com'))
+    config_file = os.getenv('CONFIG_FILE')
+    assert config_file is not None
+
+    with open(config_file) as f:
+        config = yaml.load(f)
+
+    for job in config['jobs']:
+        fn = globals()[job['type']]
+        ct = crontab(**job['schedule'])
+        sender.add_periodic_task(ct, fn.s(**job['args']))
+
+    # sender.add_periodic_task(
+    #     crontab(hour="*/1"),
+    #     notify_new_transactions.s('Rogers Mastercard', 'rogersbank.json.gpg', 'kevin.jing.qiu@gmail.com'))
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-celery -A florin_notifier.scheduler -b redis://localhost:6379 worker -B -l INFO
+CONFIG_FILE=$(pwd)/config.yaml celery -A florin_notifier.scheduler -b redis://localhost:6379 worker -B -l INFO
